d5c2dda3881fe975229f19733828f75b0f3c86cc
imai-daisuke <dasiuke.bbc@icloud.com>
2021-01-25 17:52:35 +0900

    as

---
+++ B3/park.py
@@ -0,0 +1,7 @@
+from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
+from mcplatform import *
+from functions import *
+import random as rand
+
+def make_park(level,start_x.start_y,start_z,x_size,y_size):
+    
