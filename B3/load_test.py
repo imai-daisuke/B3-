e2a13bd068dcdd3a57ef03b34ac1d9041fca33e6
imai-daisuke <dasiuke.bbc@icloud.com>
2021-01-26 23:36:47 +0900

    a

--- B3/load_test.py
+++ B3/load_test.py
@@ -1,7 +1,7 @@
 from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
 from mcplatform import *
 from functions import *
-import load as L
+import road as R
 import light
 import car
 import skyscraper as s
@@ -11,6 +11,6 @@ displayName = "OIKORA"
 
 def perform(level, box, options):
     (width, height, depth)=getBoxSize(box)
-    L.road_builder(level,box.minx,box.miny,box.minz,box.maxx-box.minx,box.maxz-box.minz)
+    R.road_builder(level,box.minx,box.miny,box.minz,box.maxx-box.minx,box.maxz-box.minz,1,1,0,0)
     #s.skyscraper(level,box.minx,box.minz,box.miny)
     #p.make_park(level,box.minx,box.miny,box.minz,30,30)
